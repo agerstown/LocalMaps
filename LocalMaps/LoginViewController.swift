1c599c67b7118784afde8f0bec5b8f7d3023d990
agerstown <nata.nikitina95@mail.ru>
2016-02-13 12:53:25 +0300

    update

--- LocalMaps/LoginViewController.swift
+++ LocalMaps/LoginViewController.swift
@@ -14,13 +14,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 //    @IBAction func buttonLoginClick(sender: AnyObject) {
